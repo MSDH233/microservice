6c1b9a274986a22cb1dbfe320f4531d2087d4080
fujinran <853804270@qq.com>
2019-10-02 19:48:32 +0800

    fjr

---
+++ springcloud/eureka7502/src/main/java/boot/Application.java
@@ -0,0 +1,19 @@
+package boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.ApplicationContext;
+
+/**
+ * Created by FuJinRan on 2019/9/29.
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication as = new SpringApplicationBuilder().build() ;
+        ApplicationContext ac = as.run(Application.class,args);
+    }
+}
