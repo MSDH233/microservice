4209a79da2d62b57b38466047456e8d5ff8c1a1f
fujinran <853804270@qq.com>
2019-12-21 17:09:16 +0800

    20191221

--- springcloud/consumer5111/src/main/java/boot/Application.java
+++ springcloud/consumer5111/src/main/java/boot/Application.java
@@ -3,8 +3,7 @@ package boot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
